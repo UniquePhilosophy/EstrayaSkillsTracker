acf703f203404d6dda02cdb75b8362906c2a00fd
UniquePhilosophy <jay-regan@hotmail.co.uk>
2024-02-03 10:35:33 +0000

    changed ssl locs in django start script

--- start_backend.bat
+++ start_backend.bat
@@ -2,4 +2,4 @@
 cd C:\Users\Administrator\Documents\EstrayaDev\EstrayaVenv\Scripts
 call .\activate
 cd C:\Users\Administrator\Documents\EstrayaDev\EstrayaProj
-python manage.py runserver_plus --cert-file localhost.pem --key-file key.pem
+python manage.py runserver_plus --cert-file localhost.crt --key-file localhost.key
